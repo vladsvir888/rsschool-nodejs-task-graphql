032a50da914b7dddc9c2d718b339b42fcd56906b
vsvir <vladsvir888@gmail.com>
2024-11-03 17:01:09 +0300

    feat: work on gql-rule test

--- src/routes/graphql/index.ts
+++ src/routes/graphql/index.ts
@@ -1,10 +1,11 @@
 import { FastifyPluginAsyncTypebox } from '@fastify/type-provider-typebox';
 import { createGqlResponseSchema, gqlResponseSchema } from './schemas.js';
-import { graphql } from 'graphql';
+import { graphql, parse, validate } from 'graphql';
 import { graphQLSchema } from './schemas.js';
+import depthLimit from 'graphql-depth-limit';
 
 const plugin: FastifyPluginAsyncTypebox = async (fastify) => {
-  const { prisma, httpErrors } = fastify;
+  const { prisma } = fastify;
 
   fastify.route({
     url: '/',
@@ -15,15 +16,21 @@ const plugin: FastifyPluginAsyncTypebox = async (fastify) => {
         200: gqlResponseSchema,
       },
     },
-    async handler(req) {
+    async handler(req, reply) {
       const { query, variables } = req.body;
+
+      const validationResult = validate(graphQLSchema, parse(query), [depthLimit(5)]);
+
+      if (validationResult.length) {
+        await reply.send({ errors: validationResult });
+      }
+
       return graphql({
         schema: graphQLSchema,
         source: query,
         variableValues: variables,
         contextValue: {
           prisma,
-          httpErrors,
         },
       });
     },
